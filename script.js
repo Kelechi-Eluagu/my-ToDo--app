337606c73f77f2377391299475aac47c355bc553
Kelechi-Eluagu <kellyeluagu@gmail.com>
2020-07-16 02:01:10 +0100

    work in progess

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ let myBtn = document.getElementById('btn');
 let myList = document.getElementsByTagName('ul');
 
 myBtn.addEventListener("click" , function() {
-    console.log("I worked");
+    if (addTask.value.length > 0) {
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(addTask.value));
+        myList.appendChild('li');
+        addTask.value = "";
+    }
 })
