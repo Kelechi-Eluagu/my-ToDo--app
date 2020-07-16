9303c34e188008e8bc1fbf86b5340a30a00bbcb9
Kelechi-Eluagu <kellyeluagu@gmail.com>
2020-07-16 02:28:44 +0100

    work in progess

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 let addTask = document.getElementById('addTask');
 let myBtn = document.getElementById('btn');
-let myList = document.getElementsByTagName('ul');
+let myList = document.querySelector('ul');
 
+debugger; 
 myBtn.addEventListener("click" , function() {
     if (addTask.value.length > 0) {
         let li = document.createElement('li');
         li.appendChild(document.createTextNode(addTask.value));
         myList.appendChild('li');
         addTask.value = "";
-    }
-})
+    };
+});
+addTask.addEventListener("keypress" , function() {
+    if(addTask.value.length > 0 && this.event.keyCode === 13) {
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(addTask.value));
+        myList.appendChild('li');
+        addTask.value = "";
+    };
+});
